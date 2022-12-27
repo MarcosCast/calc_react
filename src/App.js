b408c92bff79899c2f1efa43931fd2736a86d038
Marcos Castro <marcosmax.5@gmail.com>
2022-12-26 22:02:05 -0300

    Aplicando a versã simplificada de uma calculadora

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import './App.css';
 
-const operadores = ['*', '/', '+', '.', '-'];
+//const operadores = ['*', '/', '+', '.', '-'];
 
 function App() {
   return (
