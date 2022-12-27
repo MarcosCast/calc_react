cc331f5031912291cb3bb8711edabde36625b94a
Marcos Castro <marcosmax.5@gmail.com>
2022-12-26 22:06:18 -0300

    Ajustando com quebra de espaço

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ function App() {
   return (
     <div className="App">
       <h1>Calculadora</h1>
+      <br/>
       <label>Primeiro Numero</label>
+      <br/>
       <input type="number" />
+      <br/>
       <label>Segundo Numero</label>
+      <br/>
       <input type="number" />  
+      <br/>
+      <br/>
 
       <select>
         <option>Somar</option>
