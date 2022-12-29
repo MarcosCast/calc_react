352bdde3f9031ae201ca5f9b0223b7ac3af3bf31
Marcos Castro <marcosmax.5@gmail.com>
2022-12-28 21:25:57 -0300

    Instalando a biblioteca 'material UI'

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 //import React, { useState } from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
 //const operadores = ['*', '/', '+', '.', '-'];
 
 function App() {
   return (
     <div className="App">
-      <h1>Calculadora</h1>
-      <br/>
-      <label>Primeiro Numero</label>
-      <br/>
-      <input type="number" />
-      <br/>
-      <label>Segundo Numero</label>
-      <br/>
-      <input type="number" />  
-      <br/>
-      <br/>
-
-      <select>
-        <option>Somar</option>
-        <option>Subtrair</option>
-        <option>Multiplicar</option>
-        <option>Dividir</option>                
-      </select>   
-    </div>
+      <Calculator />
+    </div>  
   );
 }
 
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,3 +1,38 @@
 import React, { useState } from 'react';
-import { Container } from 'react-dom';
-import { Box } from "@mui/system";
+import './Calculator.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+export default function Calculator() {
+    return (
+       // <React.Fragment>
+          //  <CssBaseline />
+                <Container maxWidth="small">
+                    <Box>
+                        <div className='backgr'>
+                            <button onClick="">1</button>
+                            <button onClick="">2</button>
+                            <button onClick="">3</button>
+                            <button onClick="">4</button>
+                            <button onClick="">5</button>
+                            <button onClick="">6</button>
+                            <button onClick="">7</button>
+                            <button onClick="">8</button>
+                            <button onClick="">9</button>
+                            <button onClick="">0</button>
+                            <button onClick="">,</button>
+                            <button onClick="">AC</button>
+                            <button onClick="">+/-</button>
+                            <button onClick="">%</button>
+                            <button onClick="">/</button>
+                            <button onClick="">*</button>
+                            <button onClick="">-</button>
+                            <button onClick="">+-</button>
+                            <button onClick="">=</button>
+                        </div>
+                    </Box>
+                </Container>
+       // </React.Fragment>
+    )
+}
