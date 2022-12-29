d6bcffae2e00ab8e90e8c19a74325f86d09e5ed7
Marcos Castro <marcosmax.5@gmail.com>
2022-12-28 23:28:26 -0300

    Primeira versão funcional concluida

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,6 +6,8 @@ import {Container} from '@mui/material';
 
 export default function Calculator() {
     const[num, setNum] = useState(0); 
+    const [oldnum, setOldNum] = useState(0);
+    const [operator, setOperator] = useState();
 
     function inputNum(e) {
         var input = e.target.value
@@ -13,55 +15,88 @@ export default function Calculator() {
             setNum(input);
         } else {
             setNum(num + input);
-        }
-
-    function percent() {
-        setNum(num / 100);
-    }   
-        
+        }        
     }
 
-    function inputClear(e) {
+    function inputClear() {
         setNum(0)
     }
+    
+    function opHandler() {
+        if(num > 0) {
+            setNum(-num)
+        } else {
+            setNum(Math.abs(num))
+        }
+    }
+
+    //Corrigir calculo com percent
+    function inputPercent() {
+        setNum(num / 100);
+    }  
+
+    function result() {
+        if (operator === "/") {
+
+            setNum(parseFloat(oldnum) / parseFloat(num));
+
+          } else if (operator === "*") {
+
+            setNum(parseFloat(oldnum) * parseFloat(num));
+
+          } else if (operator === "-") {
+
+            setNum(parseFloat(oldnum) - parseFloat(num));
+
+          } else if (operator === "+") {
+
+            setNum(parseFloat(oldnum) + parseFloat(num));
+          }
+
+    }
+
+    function operar(e) {
+        var opInput = e.target.value;
+        setOperator(opInput);
+        setOldNum(num);
+        setNum(0);
+      }
+
 
     return (
         <div>
          <Box m={10} />   
-         
-           
                 <Container maxWidth="xs">
-                    
                         <div className='backgr'>
                         <Box m={12} />  
                           <h1 className="led">{num}</h1>
                             <button className='con' onClick={inputClear}>AC</button>
-                            <button className='con'>+/-</button>
-                            <button className='con' onClick={percent}>%</button>
-                            <button className='op'>/</button>
+                            <button className='con' onClick={opHandler}>+/-</button>
+                            <button className='con' onClick={inputPercent}>%</button>
+                            <button className='op' onClick={operar} value="/">/</button>
 
                             <button className='numero' onClick={inputNum} value="7">7</button>
                             <button className='numero' onClick={inputNum} value="8">8</button>
                             <button className='numero' onClick={inputNum} value="9">9</button>
 
-                            <button className='op'>*</button>
+                            <button className='op' onClick={operar} value="*">*</button>
 
                             <button className='numero' onClick={inputNum} value="4">4</button>
                             <button className='numero' onClick={inputNum} value="5">5</button>
                             <button className='numero' onClick={inputNum} value="6">6</button>
 
-                            <button className='op'>-</button>
+                            <button className='op' onClick={operar} value="-">-</button>
 
                             <button className='numero' onClick={inputNum} value="1">1</button>
                             <button className='numero' onClick={inputNum} value="2">2</button>
                             <button className='numero' onClick={inputNum} value="3">3</button>
 
-                            <button className='op'>+</button>                            
+                            <button className='op' onClick={operar} value="+">+</button>                            
                             
                             <button className='numero' onClick={inputNum} value="0">0</button>
                             <button className='numero' onClick={inputNum} value="00">00</button>
-                            <button className='numero'>,</button>                         
-                            <button >=</button>
+                            <button className='numero' onClick={inputNum} value=".">.</button>                         
+                            <button onClick={result}>=</button>
                         </div>      
                 </Container>
        </div>
